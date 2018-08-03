cc5a58dda8f6e33252f5cc431020d9b548996d0d
Donnie Zimmerman <donniez0930@gmail.com>
2018-08-02 22:17:05 -0700

    part2 challenge one has error

--- part2/server.js
+++ part2/server.js
@@ -1,6 +1,44 @@
 var express = require('express');
 var app = express();
 var port = process.env.PORT || 8000;
+var path = require('path');
+var fs = require('fs');
+
+app.post('/create/:name/:age',function(req,res){
+  let newObj = {
+    name: req.params.name,
+    age: parseInt(req.params.age)
+  }
+  fs.readFile('/storage.json','utf-8', function(err,data){
+    let dataAsArr = JSON.parse(data);
+    dataAsArr.push(newObj);
+    fs.writeFile('storage.json', JSON.stringify(dataAsArr),function(err){
+      res.sendStatus(200);
+    })
+  });
+});
+
+app.get('/',function(req,res){
+  fs.readFile('/storage.json','utf-8',function(err,data){
+    res.json(JSON.parse(data));
+  })
+});
+
+app.get('/:name',function(req,res){
+  fs.readFile('/storage.json','utf-8',function(err,data){
+    let parsedData = JSON.parse(data);
+    let matchedUser = parsedData.filter((item)=>{
+      return item.name ==req.params.name;
+    });
+    if(matchedUser.length >= 1){
+      res.json(matchedUser[0]);
+    }
+    else
+    {
+      res.sendStatus(400);
+    }
+  });
+});
 
 app.get('/yourroute', function(req, res) {
   res.send("stuff");
